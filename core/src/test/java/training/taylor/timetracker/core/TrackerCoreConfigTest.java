f9db8df664986a8e0bf15a0254ecd0339c5d1fff
girish-cicd <136220598+girish-cicd@users.noreply.github.com>
2023-06-22 10:43:03 +0530

    Update TrackerCoreConfigTest.java

--- core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerCoreConfigTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import training.taylor.timetracker.core.dao.TimeEntry;
+//import training.taylor.timetracker.core.dao.TimeEntry;
 
 import java.util.List;
 
